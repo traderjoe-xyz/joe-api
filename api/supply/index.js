49ad302c15f0b634a139d2ca4b44d10ac24712bd
Louis <louismemyself@gmail.com>
2021-08-25 21:42:25 +0200

    fixing circulating supply

--- api/supply/index.js
+++ api/supply/index.js
@@ -9,24 +9,33 @@ const BN = require('bn.js');
 const web3 = web3Factory(AVAX_CHAIN_ID);
 const joeContract = new web3.eth.Contract(JoeContactABI, joeTokenAddress);
 
-async function circulatingSupply(ctx) {
-  ctx.body = new BN(await getCirculatingSupply()).div(new BN(2)).toString();
-}
 
-async function getCirculatingSupply() {
+async function getTotalSupply() {
   return await joeContract.methods.totalSupply().call();
-}
-
-async function maxSupply(ctx) {
-  ctx.body = await getMaxSupply();
 }
 
 async function getMaxSupply() {
   return await joeContract.methods.maxSupply().call();
 }
 
+async function getBalanceOf(address) {
+  return await joeContract.methods.balanceOf(address).call();
+}
+
+async function circulatingSupply(ctx) {
+  var develeopmentFunds = new BN(await getBalanceOf("0xaFF90532E2937fF290009521e7e120ed062d4F34"));
+  var foundationFunds = new BN(await getBalanceOf("0x66Fb02746d72bC640643FdBa3aEFE9C126f0AA4f"));
+  var strategicInvestorFunds = new BN(await getBalanceOf("0xc13B1C927565C5AF8fcaF9eF7387172c447f6796"));
+  ctx.body = new BN(await getTotalSupply()).sub(develeopmentFunds).sub(foundationFunds).sub(strategicInvestorFunds).toString();
+}
+
+async function maxSupply(ctx) {
+  ctx.body = await getMaxSupply();
+}
+
+
 async function totalSupply(ctx) {
-  ctx.body = await getCirculatingSupply();
+  ctx.body = await getTotalSupply();
 }
 
 module.exports = {circulatingSupply, totalSupply, maxSupply};

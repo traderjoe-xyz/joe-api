71acd86cf12fc07ee1112a50e417c2bd1222accd
Louis <louismemyself@gmail.com>
2021-09-22 19:10:59 +0200

    fix file

--- api/price/index.js
+++ api/price/index.js
@@ -104,7 +104,6 @@ async function priceOfToken(ctx) {
     if (!("tokenAddress" in ctx.params))
         ctx.body = ""
     else {
-        const tokenList = require('../../../loopedEvents/tokenList.json')
         try {
             if (ctx.params.tokenAddress in tokenList) {
                 tokenAddress = tokenList[ctx.params.tokenAddress]
@@ -125,7 +124,6 @@ async function derivedPriceOfToken(ctx) {
     if (!("tokenAddress" in ctx.params))
         ctx.body = ""
     else {
-        const tokenList = require('../../../loopedEvents/tokenList.json')
         try {
             if (ctx.params.tokenAddress in tokenList) {
                 tokenAddress = tokenList[ctx.params.tokenAddress]

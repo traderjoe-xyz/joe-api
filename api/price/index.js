0f3a8247a19f25b010badf13f28e59b59433495a
Louis <louismemyself@gmail.com>
2021-09-24 01:05:55 +0200

    add cached datas, fix max supply

--- api/price/index.js
+++ api/price/index.js
@@ -22,29 +22,122 @@ const wavaxUsdtePair = "0xeD8CBD9F0cE3C6986b22002F03c6475CEb7a6256"
 const wavaxUsdcePair = "0xA389f9430876455C36478DeEa9769B7Ca4E3DDB1"
 
 // contracts
-const usdceContract = getContractAsERC20(usdceTokenAddress);
-const usdteContract = getContractAsERC20(usdteTokenAddress);
-const wavaxContract = getContractAsERC20(wavaxTokenAddress);
 const joefactory_contract = new web3.eth.Contract(JoeFactoryContractABI, joeFactoryAddress)
 
 // constants
 const E18 = new BN("10").pow(new BN("18"))
 const EIGHTEEN = new BN("18")
 const TWO = new BN("2")
+const zeroAddress = "0x0000000000000000000000000000000000000000"
+
+
+class Cache {
+    minElapsedTimeInMs = 60000; // 60 seconds
+
+    constructor() {
+        this.pair = {}
+        this.decimals = {}
+        this.contract = {}
+        this.cachedPrice = {}
+    }
+
+    async getPair(tokenAddress) {
+        if (tokenAddress in this.pair) {
+            return this.pair[tokenAddress]
+        }
+        console.log("getPair", tokenAddress)
+        const pairAddress = await getPairAddress(tokenAddress)
+
+        if (pairAddress === zeroAddress) {
+            return pairAddress
+        }
+
+        this.pair[tokenAddress] = pairAddress
+        return pairAddress
+    }
+
+    async getDecimals(tokenAddress) {
+        if (tokenAddress in this.decimals) {
+            return this.decimals[tokenAddress]
+        }
+        console.log("getDecimals", tokenAddress)
+        const decimals = await this.getContract(tokenAddress).methods.decimals().call()
+
+        this.decimals[tokenAddress] = decimals
+        return decimals
+    }
+
+    getContract(tokenAddress) {
+        if (tokenAddress in this.contract) {
+            return this.contract[tokenAddress]
+        }
+        console.log("getContract", tokenAddress)
+        const tokenContract = getContractAsERC20(tokenAddress)
+        this.contract[tokenAddress] = tokenContract
+        return tokenContract
+    }
+
+    async getAvaxPrice() {
+        if (wavaxTokenAddress in this.cachedPrice) {
+            if (this.cachedPrice[wavaxTokenAddress].lastRequestTimestamp + this.minElapsedTimeInMs > Date.now()) {
+                return this.cachedPrice[wavaxTokenAddress].lastResult
+            }
+        }
+        console.log("getAvaxPrice")
+        const reservesUSDCE = await getReserves(wavaxTokenAddress, usdceTokenAddress, wavaxUsdcePair)
+        const reservesUSDTE = await getReserves(wavaxTokenAddress, usdteTokenAddress, wavaxUsdtePair)
+
+        const priceUSDCE = reservesUSDCE.reserveToken1.mul(E18).div(reservesUSDCE.reserveToken0)
+        const priceUSDTE = reservesUSDTE.reserveToken1.mul(E18).div(reservesUSDTE.reserveToken0)
+
+        const avaxPrice = priceUSDCE.add(priceUSDTE).div(TWO)
+
+        const lastRequestTimestamp = Date.now()
+        const lastResult = avaxPrice
+        this.cachedPrice[wavaxTokenAddress] = {lastRequestTimestamp, lastResult}
+
+        return avaxPrice
+    }
+
+    async getPrice(tokenAddress, derived) {
+        if (!(tokenAddress in this.cachedPrice) ||
+            this.cachedPrice[tokenAddress].lastRequestTimestamp + this.minElapsedTimeInMs < Date.now() // check if price needs to be updated
+        ) {
+            console.log("getPrice", tokenAddress, derived)
+            const pairAddress = await cache.getPair(tokenAddress)
+
+            if (pairAddress === zeroAddress) {
+                throw 'Error: Given address "' + tokenAddress + '" isn\'t paired with WAVAX on TraderJoe.'
+            }
+
+            const reserves = await getReserves(wavaxTokenAddress, tokenAddress, pairAddress)
+
+            const price = reserves.reserveToken0.mul(E18).div(reserves.reserveToken1)
+
+            const lastRequestTimestamp = Date.now()
+            const lastResult = price
+            this.cachedPrice[tokenAddress] = {lastRequestTimestamp, lastResult}
+        }
+
+        return derived ?
+            this.cachedPrice[tokenAddress].lastResult :
+            (await this.getAvaxPrice()).mul(this.cachedPrice[tokenAddress].lastResult).div(E18)
+    }
+}
 
 function getContractAsERC20(tokenAddress) {
     return new web3.eth.Contract(ERC20ContractABI, tokenAddress)
 }
 
-async function getReserves(token0Contract, token1Contract, pairAddress) {
-    const results = await Promise.all([
-        token0Contract.methods.decimals().call(),
-        token1Contract.methods.decimals().call(),
-        token0Contract.methods.balanceOf(pairAddress).call(),
-        token1Contract.methods.balanceOf(pairAddress).call()
-    ])
-    const reserveToken0 = new BN(results[2]).mul(get10PowN(EIGHTEEN.sub(new BN(results[0]))))
-    const reserveToken1 = new BN(results[3]).mul(get10PowN(EIGHTEEN.sub(new BN(results[1]))))
+async function getReserves(token0Address, token1Address, pairAddress) {
+    const balanceToken0 = await cache.getContract(token0Address).methods.balanceOf(pairAddress).call()
+    const balanceToken1 = await cache.getContract(token1Address).methods.balanceOf(pairAddress).call()
+
+    const decimalsToken0 = await cache.getDecimals(token0Address)
+    const decimalsToken1 = await cache.getDecimals(token1Address)
+
+    const reserveToken0 = new BN(balanceToken0).mul(get10PowN(EIGHTEEN.sub(new BN(decimalsToken0))))
+    const reserveToken1 = new BN(balanceToken1).mul(get10PowN(EIGHTEEN.sub(new BN(decimalsToken1))))
     return {reserveToken0, reserveToken1}
 }
 
@@ -52,50 +145,12 @@ function get10PowN(n) {
     return new BN("10").pow(new BN(n.toString()))
 }
 
-async function getDerivedPriceOfPair(token0Contract, token1Contract, pairAddress, fromToken0) {
-    const reserves = await getReserves(token0Contract, token1Contract, pairAddress)
-    return (fromToken0 === true ?
-            reserves.reserveToken1.mul(E18).div(reserves.reserveToken0) :
-            reserves.reserveToken0.mul(E18).div(reserves.reserveToken1)
-    )
-}
-
-async function getAvaxPrice() {
-    const results = await Promise.all([
-        getDerivedPriceOfPair(wavaxContract, usdceContract, wavaxUsdcePair, true),
-        getDerivedPriceOfPair(wavaxContract, usdteContract, wavaxUsdtePair, true)
-    ])
-    return new BN(results[0].add(results[1]).div(TWO).toString())
-}
-
-async function getPrice(tokenAddress) {
-    const results = await Promise.all([
-        getDerivedPrice(tokenAddress),
-        getAvaxPrice()
-    ])
-    return results[0].mul(results[1]).div(E18)
-}
-
-async function getDerivedPrice(tokenAddress) {
-    const results = await Promise.all([
-        getContractAsERC20(tokenAddress),
-        await getPairAddress(tokenAddress)
-    ])
-
-    if (results[1] === "0x0000000000000000000000000000000000000000") {
-        let symbol;
-        try {
-            symbol = await results[0].methods.symbol().call()
-            throw 'Error: There is no ' + symbol + '/WAVAX pair on TraderJoe.'
-        } catch (e) {
-            if (e === 'Error: There is no ' + symbol + '/WAVAX pair on TraderJoe.')
-                throw e
-            else
-                throw 'Error: Given address "' + tokenAddress + '" isn\'t paired with WAVAX on TraderJoe.'
-        }
+async function getPrice(tokenAddress, derived) {
+    if (tokenAddress === wavaxTokenAddress) {
+        return await cache.getAvaxPrice()
     }
-    const derivedPrice = await getDerivedPriceOfPair(wavaxContract, results[0], results[1], false)
-    return new BN(derivedPrice)
+
+    return await cache.getPrice(tokenAddress, derived)
 }
 
 
@@ -109,7 +164,7 @@ async function getPairAddress(tokenAddress) {
     )
 }
 
-async function priceOfToken(ctx) {
+async function logics(ctx, derived) {
     let tokenAddress;
     if (!("tokenAddress" in ctx.params))
         ctx.body = ""
@@ -121,34 +176,27 @@ async function priceOfToken(ctx) {
                 tokenAddress = web3.utils.toChecksumAddress(ctx.params.tokenAddress)
             }
 
-            tokenAddress === wavaxTokenAddress ?
-                ctx.body = (await getAvaxPrice()).toString() :
-                ctx.body = (await getPrice(tokenAddress)).toString()
+            console.log("logics", tokenAddress)
+
+            derived ?
+                tokenAddress === wavaxTokenAddress ?
+                    ctx.body = E18.toString() :
+                    ctx.body = (await getPrice(tokenAddress, derived)).toString() :
+                ctx.body = (await getPrice(tokenAddress, derived)).toString()
+
         } catch (e) {
             ctx.body = e.toString()
         }
     }
+}
+
+async function priceOfToken(ctx) {
+    await logics(ctx, false)
 }
 
 async function derivedPriceOfToken(ctx) {
-    let tokenAddress;
-    if (!("tokenAddress" in ctx.params))
-        ctx.body = ""
-    else {
-        try {
-            if (ctx.params.tokenAddress in tokenList) {
-                tokenAddress = tokenList[ctx.params.tokenAddress]
-            } else {
-                tokenAddress = web3.utils.toChecksumAddress(ctx.params.tokenAddress)
-            }
-
-            tokenAddress === wavaxTokenAddress ?
-                ctx.body = E18.toString() :
-                ctx.body = (await getDerivedPrice(tokenAddress)).toString()
-        } catch (e) {
-            ctx.body = e.toString()
-        }
-    }
+    await logics(ctx, true)
 }
 
+const cache = new Cache()
 module.exports = {priceOfToken, derivedPriceOfToken};
--- api/supply/index.js
+++ api/supply/index.js
@@ -1,41 +1,81 @@
 'use strict';
 
 const {web3Factory} = require("../../utils/web3");
-const ERC20ContractABI = require('../../abis/ERC20ContractABI.json');
+const JoeContractABI = require('../../abis/JoeTokenContractABI.json');
 const {AVAX_CHAIN_ID} = require("../../constants");
 const joeTokenAddress = "0x6e84a6216eA6dACC71eE8E6b0a5B7322EEbC0fDd"
 const BN = require('bn.js');
 
 const web3 = web3Factory(AVAX_CHAIN_ID);
-const joeContract = new web3.eth.Contract(ERC20ContractABI, joeTokenAddress);
+const joeContract = new web3.eth.Contract(JoeContractABI, joeTokenAddress);
 
 
-async function getTotalSupply() {
-  return await joeContract.methods.totalSupply().call();
-}
+class Cache {
+    minElapsedTimeInMs = 10000; // 10 seconds
 
-async function getMaxSupply() {
-  return await joeContract.methods.maxSupply().call();
+    constructor() {
+        this.cachedCirculatingSupply = undefined
+        this.cachedMaxSupply = undefined
+        this.cachedTotalSupply = undefined
+    }
+
+
+    async getTotalSupply() {
+        if (!this.cachedTotalSupply ||
+            this.cachedTotalSupply.lastRequestTimestamp + this.minElapsedTimeInMs < Date.now() // check if supply needs to be updated
+        ) {
+            console.log("getTotalSupply")
+            const totalSupply = new BN(await joeContract.methods.totalSupply().call());
+            const lastRequestTimestamp = Date.now();
+            this.cachedTotalSupply = {totalSupply, lastRequestTimestamp}
+        }
+
+        return this.cachedTotalSupply.totalSupply
+    }
+
+    async getMaxSupply() {
+        if (!this.cachedMaxSupply) {
+            console.log("getMaxSupply")
+            const maxSupply = new BN(await joeContract.methods.maxSupply().call());
+            const lastRequestTimestamp = Date.now();
+            this.cachedMaxSupply = {maxSupply, lastRequestTimestamp}
+        }
+        return this.cachedMaxSupply.maxSupply
+    }
+
+    async getCirculatingSupply() {
+        if (!this.cachedCirculatingSupply ||
+            this.cachedCirculatingSupply.lastRequestTimestamp + this.minElapsedTimeInMs < Date.now() // check if supply needs to be updated
+        ) {
+            console.log("getCirculatingSupply")
+            const developmentFunds = new BN(await getBalanceOf("0xaFF90532E2937fF290009521e7e120ed062d4F34"));
+            const foundationFunds = new BN(await getBalanceOf("0x66Fb02746d72bC640643FdBa3aEFE9C126f0AA4f"));
+            const strategicInvestorFunds = new BN(await getBalanceOf("0xc13B1C927565C5AF8fcaF9eF7387172c447f6796"));
+            const circulatingSupply = (await this.getTotalSupply()).sub(developmentFunds).sub(foundationFunds).sub(strategicInvestorFunds);
+
+            const lastRequestTimestamp = Date.now();
+            this.cachedCirculatingSupply = {circulatingSupply, lastRequestTimestamp}
+        }
+        return this.cachedCirculatingSupply.circulatingSupply
+    }
 }
 
 async function getBalanceOf(address) {
-  return await joeContract.methods.balanceOf(address).call();
+    return await joeContract.methods.balanceOf(address).call();
 }
 
 async function circulatingSupply(ctx) {
-  var develeopmentFunds = new BN(await getBalanceOf("0xaFF90532E2937fF290009521e7e120ed062d4F34"));
-  var foundationFunds = new BN(await getBalanceOf("0x66Fb02746d72bC640643FdBa3aEFE9C126f0AA4f"));
-  var strategicInvestorFunds = new BN(await getBalanceOf("0xc13B1C927565C5AF8fcaF9eF7387172c447f6796"));
-  ctx.body = new BN(await getTotalSupply()).sub(develeopmentFunds).sub(foundationFunds).sub(strategicInvestorFunds).toString();
+    ctx.body = (await cache.getCirculatingSupply()).toString();
 }
 
 async function maxSupply(ctx) {
-  ctx.body = await getMaxSupply();
+    ctx.body = (await cache.getMaxSupply()).toString();
 }
 
 
 async function totalSupply(ctx) {
-  ctx.body = await getTotalSupply();
+    ctx.body = (await cache.getTotalSupply()).toString();
 }
 
+const cache = new Cache()
 module.exports = {circulatingSupply, totalSupply, maxSupply};
